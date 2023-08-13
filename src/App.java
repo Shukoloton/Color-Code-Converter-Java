79eaeb897d5c593b28b3f9f8677aab18377ad184
Shukoloton <141661300+Shukoloton@users.noreply.github.com>
2023-08-13 14:50:58 +0200

    Created v0.0.4-nonfull.
    Added ability to choose another color code after finishing conversion. Return to color code choosing not available as of yet, but planned as v0.0.5-nonfull.

---
+++ src/App.java
@@ -0,0 +1,165 @@
+//Color Code Converter v0.0.4-nonfull.
+//Created by Shukoloton (https://github.com/Shukoloton/Color-Code-Converter-Java).
+
+//Imports scanner utility.
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        
+        //Creates scanners.
+        Scanner inputDecide = new Scanner(System.in);
+        Scanner hexScanner = new Scanner(System.in);
+        Scanner redScanner = new Scanner(System.in);
+        Scanner blueScanner = new Scanner(System.in);
+        Scanner greenScanner = new Scanner(System.in);
+        Scanner backScanner = new Scanner(System.in);
+        Scanner returnScanner = new Scanner(System.in);
+        
+        //Header.
+        System.out.println("Color Code Converter v0.0.4-nonfull.");
+        System.out.println("Created by Shukoloton");
+
+        //Creates strings for colours.
+        String ANSI_RED = "\u001B[31m";
+        String ANSI_RESET = "\u001B[0m";
+        
+        //Prints prompt and activates inputDecide scanner on next line.
+        System.out.println("Convert from (hex or RGB):");
+        String rgbOrHex = inputDecide.nextLine();
+        
+        //Infinite while loop.
+        while (true) {
+
+            //Infinite while loop breaks if rgbOrHex is equal to rgb, RGB, hex, or HEX.
+            if (rgbOrHex.equals("rgb") || rgbOrHex.equals("RGB") || rgbOrHex.equals("hex") || rgbOrHex.equals("HEX")) {
+                break;
+            }  
+
+            //If the while loop didn't break before, the program prints a line prompting the user
+            //to input hex or rgb once again.
+            System.out.println("Invalid input. Please enter" + ANSI_RED + " " + "VALID" + " " + ANSI_RESET + "input or write exit to exit program:");
+            System.out.println("Valid inputs: rgb, RGB, hex, HEX.");
+            rgbOrHex = inputDecide.nextLine();
+
+            //Infinite while loop breaks if rgbOrHex is equal to rgb, RGB, hex, or HEX.
+            if (rgbOrHex.equals("rgb") || rgbOrHex.equals("RGB") || rgbOrHex.equals("hex") || rgbOrHex.equals("HEX")) {
+                break;
+            }
+
+            //Infinite while loop breaks if rgbOrHex is equal to exit.
+            if (rgbOrHex.equals("exit")) {
+                System.exit(0);
+            }
+        }
+
+        //Checks whether the user wants to convert from hex or RGB.
+        if (rgbOrHex.equals("hex") || rgbOrHex.equals("HEX")) {
+
+            //While loop that serves to facilitate the back function.
+            while (true) {
+                
+                //Prints prompt to enter hex value and activates hexScanner on next line.
+                System.out.println("Input hex value:");
+                String hexColor = hexScanner.nextLine();
+
+                    //Creates string of just the hex value from the last 6 characters of hexColor.
+                    String rawHexColor = hexColor.substring(hexColor.length() - 6);
+
+                    //Takes substrings of rawHexColor and converts them to RGB values.
+                    int rgbRed = Integer.valueOf(rawHexColor.substring(0,2), 16);
+                    int rgbBlue = Integer.valueOf(rawHexColor.substring(2,4), 16);
+                    int rgbGreen = Integer.valueOf(rawHexColor.substring(4,6), 16);
+
+                    //Prints the RGB value in parentheses with commas.
+                    System.out.println("(" + rgbRed + ", " + rgbBlue + ", " + rgbGreen + ")");
+
+                    //Reads usert input using backScanner.
+                    String backString = backScanner.nextLine();
+                    
+
+                        //If user input is back, Back, or BACK, returns to beginning of block and allows the user to convert another hex value.
+                        if (backString.equals("back") || backString.equals("Back") || backString.equals("BACK")) {
+                            continue;
+                        
+                        //If the user inputs Enter, quit, close, stop, or exit, the program closes.
+                        } else if (backString.equals("") || backString.equals("quit") || backString.equals("close") || backString.equals("stop") || backString.equals("exit")) {
+                            System.exit(0);
+                        }
+            }
+
+        //Occurs if the user doesn't input hex or HEX.
+        } else if (rgbOrHex.equals("rgb") || rgbOrHex.equals("RGB")){
+
+            //While loop that serves to facilitate the back function.
+            while (true) {
+
+                //Asks for red value and activates redScanner on next line (integer).
+                System.out.println("Input R (red) value:");
+                int redInput = redScanner.nextInt();
+
+                //Asks for green value and activates greenScanner on next line (integer).
+                System.out.println("Input G (green) value:");
+                int greenInput = greenScanner.nextInt();
+
+                //Asks for blue value and activates blueScanner on next line (integer).
+                System.out.println("Input B (blue) value:");
+                int blueInput = blueScanner.nextInt();
+
+                //Converts from decimal to hexadecimal.
+                String redOutput = Integer.toHexString(redInput);
+                String greenOutput = Integer.toHexString(greenInput);
+                String blueOutput = Integer.toHexString(blueInput);
+                    
+                //Takes length of hexadecimal strings.
+                Integer redLength = redOutput.length();
+                Integer greenLength = greenOutput.length();
+                Integer blueLength = blueOutput.length();
+
+                //Creates adjusted variables
+                String redAdjusted;
+                String greenAdjusted;
+                String blueAdjusted;
+
+                    //Checks whether the length of redOutput is equal to zero.
+                    //If yes, adds a leading zero to the code. If not, proceeds.
+                    if (redLength == 1) {
+                        redAdjusted = "0" + redOutput;
+                    } else {
+                        redAdjusted = redOutput;
+                    }
+                    
+                    //Checks whether the length of greenOutput is equal to zero.
+                    //If yes, adds a leading zero to the code. If not, proceeds.
+                    if (greenLength == 1) {
+                        greenAdjusted = "0" + greenOutput;
+                    } else {
+                        greenAdjusted = greenOutput;
+                    }
+                    
+                    //Checks whether the length of blueOutput is equal to zero.
+                    //If yes, adds a leading zero to the code. If not, proceeds.
+                    if (blueLength == 1) {
+                        blueAdjusted = "0" + blueOutput;
+                    } else {
+                        blueAdjusted = blueOutput;
+                    }
+                
+                //Prints the hex value prexifed with a hashtag.
+                System.out.println("#" + redAdjusted + greenAdjusted + blueAdjusted);
+                
+                //Reads user input using the backScanner.
+                String backString = backScanner.nextLine();
+
+                    //If user input is back, Back, or BACK, returns to beginning of block and allows the user to convert another hex value.
+                    if (backString.equals("back") || backString.equals("Back") || backString.equals("BACK")) {
+                        continue;
+
+                    //If the user inputs Enter, quit, close, stop, or exit, the program closes.
+                    } else if (backString.equals("") || backString.equals("quit") || backString.equals("close") || backString.equals("stop") || backString.equals("exit")) {
+                        System.exit(0);
+                    }
+            }
+        }
+    }
+}
